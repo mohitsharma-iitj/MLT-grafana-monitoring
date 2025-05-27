6026d701d669ebaf1c0743afbf87af2a8b7901d0
Mohit Sharma <sharma.132@iitj.ac.in>
2025-05-27 11:21:19 +0530

    tracer step1

--- webapi-dj/manage.py
+++ webapi-dj/manage.py
@@ -2,8 +2,32 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from opentelemetry import trace
+from opentelemetry.exporter.jaeger.thrift import JaegerExporter
+from opentelemetry.instrumentation.django import DjangoInstrumentor
+from opentelemetry.instrumentation.logging import LoggingInstrumentor
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+
 
 def main():
+
+    # DjangoInstrumentor().instrument()
+    # LoggingInstrumentor().instrument()
+    # RequestsInstrumentor().instrument()
+
+    # jaeger_exporter = JaegerExporter(
+    #     agent_host_name=os.getenv("TRACING_HOST"),
+    #     agent_port= int(os.getenv("TRACING_PORT")),
+    # )
+    # trace.set_tracer_provider(TracerProvider(
+    #     resource=Resource.create({SERVICE_NAME: 'webapp'})
+    # ))
+    # span_processor = BatchSpanProcessor(jaeger_exporter)
+    # trace.get_tracer_provider().add_span_processor(span_processor)
+
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webapi.settings')
 
